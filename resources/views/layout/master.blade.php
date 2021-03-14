c0a03c52d25d2211e5e5b985a053d126857d9e1a
RezaParsian <RezaParsian76@gmail.com>
2021-03-14 22:03:52 +0330

    make some meta tag

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -2,9 +2,26 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="title" content="اندرویدنویسان">
+    <meta name="description" content="اندرویدنویسان">
+    <meta name="keywords" content="برنامه نویسی,رضا پارسیان,سهراب فلاح زاده,سورس,کیبورد ازاد,api,برنامه ازاد,open sorce,خون قلم,سیشارپ,php,C#,جاوا اسکریپت,افزونه,MQL,فارکس,forex,sql,دیتابیس,داده کاوی">
+    <meta name="robots" content="index, follow">
+    <meta name="language" content="English">
+    <meta name="author" content="رضا پارسیان و سهراب فلاح زاده">
+
+    <meta property="og:title" content="اندرویدنویسان">
+    <meta property="og:description" content="اندرویدنویسان">
+    <meta property="og:image" content="https://androidnevisan.ir/favicon.ico">
+    <meta property="og:url" content="https://androidnevisan.ir/">
+
+    <meta name="twitter:title" content="اندرویدنویسان">
+    <meta name="twitter:description" content="اندرویدنویسان">
+    <meta name="twitter:image" content="https://androidnevisan.ir/favicon.ico">
+
+    <link rel="shortcut icon" href="{{asset("favicon.ico")}}">
+
     <title>{{ env('APP_NAME') }} @yield('ex-title')</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
