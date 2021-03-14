1090b8fe8b7cf85682d2d651a3c2ec1f698111fc
RezaParsian <RezaParsian76@gmail.com>
2021-03-14 20:36:46 +0330

    site is ready

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,26 +5,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ env('APP_NAME') }}</title>
+    <title>{{ env('APP_NAME') }} @yield('ex-title')</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
 <body>
-    <div class="container">
+    <div class="container" id="app">
         <nav class="navbar navbar-expand-sm navbar-light">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <!-- Brand/logo -->
             <a class="navbar-brand ml2" href="#">
-                <img src="{{ asset('/img/logo.png') }}" alt="logo" style="width:40px;" class="rounded">
+                <img src="{{ asset('/img/logo.png') }}" alt="{{ env('APP_NAME') }}" style="width:5rem;" class="rounded">
             </a>
             <div class="collapse navbar-collapse" id="collapsibleNavbar">
                 <ul class="navbar-nav mr-3">
-                    <li class="nav-item cool-link">
+                    <li class="mx-2 nav-item cool-link active">
                         <a class="nav-link" href="#">خانه</a>
                     </li>
-                    <li class="nav-item cool-link">
+                    <li class="mx-2 nav-item cool-link">
                         <a class="nav-link" href="#">تماس با من</a>
                     </li>
                 </ul>
@@ -33,16 +33,14 @@
 
         <div class="row mx-auto">
             <div class="col align-self-center mx-5">
-                <h1>ایران بدن</h1>
-                <p class="ml-5 mt-2">محتوای به‌روز و سامانه قدرتمند برای مدیران و مربیان باشگاه‌های بدنسازی و فیتنس،
+                <h1 class="text-right">ایران بدن</h1>
+                <p class="text-right ml-5 mt-2">محتوای به‌روز و سامانه قدرتمند برای مدیران و مربیان باشگاه‌های بدنسازی و
+                    فیتنس،
                     مربیان آنلاین،
                     کلینیک‌های ورزش و تغذیه، و ورزشکاران</p>
                 <div class="col-md mx-auto">
                     <div class="col-md d-flex justify-content-center my-1">
-                        <button class="btn btn-outline-success col-md-4">Button 1</button>
-                    </div>
-                    <div class="col-md d-flex justify-content-center my-1">
-                        <button class="btn btn-outline-danger col-md-4">Button 2</button>
+                        <button class="btn btn-success rounded-pill col-md-4">بیشتر</button>
                     </div>
                 </div>
             </div>
@@ -50,16 +48,8 @@
                 <img src="{{ asset('img/Sohrab_3.png') }}" alt="Sohrab_3" class="img-fluid mx-auto">
             </div>
         </div>
-
-        <div class="row mx-auto">
-            <div class="deb_service_block ">
-                <i class="fa fa-eye"></i>
-                <h4>مدیر و مربی باشگاه</h4>
-                <p>دیگه وقتشه خدماتت رو بصورت آنلاین و مدرن ارائه بدی!</p>
-            </div>
-        </div>
     </div>
+
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
-
 </html>
